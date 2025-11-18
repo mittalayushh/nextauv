512c7540aa9b17c43d7882da61f84c098061dfeb
Ayush Mittal <mittalayushh346@gmail.com>
2025-11-18 13:40:25 +0530

    fixed issues, recommiting from root folder

--- app/login/page.js
+++ app/login/page.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      const res = await fetch("http://localhost:4000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
