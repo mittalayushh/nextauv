faba5b25948514910b126ec775706002b4ffe0ce
Ayush Mittal <mittalayushh346@gmail.com>
2025-12-02 11:24:41 +0530

    fixed

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -21,22 +21,9 @@ export default function LoginPage() {
       const data = await res.json();
       if (res.ok) {
         localStorage.setItem("token", data.token);
-        localStorage.setItem("email", form.email);
-        // Get username from decoded token
-        try {
-          const base64Url = data.token.split(".")[1];
-          const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-          const jsonPayload = decodeURIComponent(
-            atob(base64)
-              .split("")
-              .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-              .join("")
-          );
-          const decoded = JSON.parse(jsonPayload);
-          localStorage.setItem("username", decoded.username);
-        } catch (err) {
-          console.error("Failed to decode token:", err);
-        }
+        localStorage.setItem("email", data.user.email);
+        localStorage.setItem("username", data.user.username);
+
         setMessage("Login successful");
         router.replace("/");
       } else {
